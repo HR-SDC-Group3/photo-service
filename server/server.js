a79e11d5cccab146b8e6e18a83f29c6b92f9829c
Your Name <you@example.com>
2018-12-21 16:26:34 -0800

    Set-up server with initial GET request to MongoDB.

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const db = require('../database/index.js');
+
+const app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.get('/photos/restaurantName', (req, res) => {
+  Promise.resolve(db.find())
+    .then((results) => {
+      res.status(200).send(results);
+    }).catch((error) => {
+      console.log(error);
+    });
+});
+
+const port = 3003;
+app.listen(port, () => {
+  console.log(`Currently listening in on port ${port}, FEC project`);
+});
