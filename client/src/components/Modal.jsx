b1484b8138d9a47afa24d43267110f5689c4a397
Your Name <you@example.com>
2018-12-26 19:15:31 -0800

    Partically finished photo Modal rendering. Modal hides website content and requires a fix

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -8,15 +8,20 @@ const Modal = (props) => {
     bottom: 0,
     left: 0,
     right: 0,
-    backgroundColor: 'rgba(0,0,0,0.3)',
+    backgroundColor: 'rgba(0,0,0,.05)',
     padding: 50,
+    zIndex: 1000,
+  }
+
+  const imageStyle = {
+    textAlign: 'center',
   }
 
   const modalStyle = {
     backgroundColor: '#fff',
     borderRadius: 5,
     maxWidth: 500,
-    minHeight: 300,
+    minHeight: 500,
     margin: '0 auto',
     padding: 30,
     position: 'relative',
@@ -28,14 +33,38 @@ const Modal = (props) => {
   }
 
   return (
-    <div style={backdropStyle}>
-      <div style={modalStyle}>
-        <div style={footerStyle}>
-          <button>Close</button>
+    props.userPhotos.map((userPhoto) => {
+      return (
+        <div style={backdropStyle}>
+          <div style={modalStyle}>
+            <button onClick={props.onClick}>Close</button>
+            <img src={userPhoto.photoURL} style={imageStyle} id="carousel-modal"></img>
+            <div style={footerStyle}>
+              <span className="carousel-description">{userPhoto.description}</span>
+              <span className="carousel-username">{userPhoto.username}</span>
+              <span className="carousel-date">{userPhoto.date}</span>
+              <button onClick={props.onClick}>Previous</button>
+              <button onClick={props.onClick}>Next</button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )
+    })
   )
+
+  // return (
+  //   <div style={backdropStyle}>
+  //         <div style={modalStyle}>
+  //           <img src={userPhoto.photoURL} style={imageStyle}></img>
+  //           <div style={footerStyle}>
+  //             <span className="carousel-description">{userPhoto.description}</span>
+  //             <span className="carousel-username">{userPhoto.username}</span>
+  //             <span className="carousel-date">{userPhoto.date}</span>
+  //             <button onClick={props.onClick}>Close</button>
+  //           </div>
+  //         </div>
+  //       </div>
+  // )
 }
 
 export default Modal;
