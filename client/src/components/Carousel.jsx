970019ef6c811831f22644ec91a46d1f6d5b6514
Your Name <you@example.com>
2018-12-26 17:17:31 -0800

    Removed comments

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -4,9 +4,6 @@ import url from 'url';
 import RestaurantPhotos from './RestaurantPhotos.jsx';
 import sampleArray from './sampledata.js';
 
-//Carousel is only concerned with rendering the RestaurantPhotos array
-//Carousel will grab the restaurant ID, and submit a request to Mongo to get all photos associated w/ that restaurant
-//Carousel will then pass down the array to restaurants to render
 class Carousel extends React.Component {
   constructor(props) {
     super(props)
@@ -27,18 +24,3 @@ class Carousel extends React.Component {
 }
 
 export default Carousel;
-
-//   return(
-//   <div className = "App" >
-//       <input type="button"
-//         onClick={this.showModal}
-//         value="Show Modal" />
-
-//       <Modal show={this.state.isOpen} onClose={this.showModal}>
-//         <RestaurantPhotos photos={this.state.userPhotos} />
-//       </Modal>
-
-
-//   </div>
-// )
-// }
--- client/src/components/Carousel.test.js
+++ client/src/components/Carousel.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Enzyme, shallow } from 'enzyme';
 import Adaptor from 'enzyme-adapter-react-16';
-// import App from '../App.jsx';
 
 describe('<CarouselContainer />', () => {
   it('Renders a Carousel container component instance', () => {
@@ -23,42 +22,3 @@ describe('<CarouselContainer />', () => {
   });
 
 });
-
-
-// describe('<CarouselContainer />', () => {
-//   it('Renders a <CarouselContainer /> component instance', () => {
-//     const carousel = shallow(<CarouselContainer />);
-//   });
-
-//   expect(carousel).to.exist;
-// });
-
-
-
-// describe('<MyComponent />', () => {
-//   it('Renders three <Foo /> components', () => {
-//     const wrapper = shallow(<MyComponent />);
-//     expect(wrapper.find(Foo)).to.have.lengthOf(3);
-//   });
-
-//   it('renders an `.icon-star`', () => {
-//     const wrapper = shallow(<MyComponent />);
-//     expect(wrapper.find(Foo)).to.have.lengthOf(3);
-//   })
-
-//   it('renders children when passed in', () => {
-//     const wrapper = shallow((
-//       <MyComponent>
-//         <div clasName="unique" />
-//       </MyComponent>
-//     ));
-//     expect(wrapper.contains(<div className="unique" />)).to.equal(true);
-//   });
-
-//   it('stimulates click events', () => {
-//     const onButtonClick = sinon.spy();
-//     const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
-//     wrapper.find('button').stimulate('click');
-//     expect(onButtonClick).to.have.property('callCount', 1);
-//   })
-// });
