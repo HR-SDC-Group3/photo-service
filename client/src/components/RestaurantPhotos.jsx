7affe119f88888b2ad9a82375a56e01fcb4875c2
Your Name <you@example.com>
2018-12-30 11:56:05 -0800

    Update Modal functionality to include passing in the photo that was clicked, and updated click handlers.

--- client/src/components/RestaurantPhotos.jsx
+++ client/src/components/RestaurantPhotos.jsx
@@ -7,39 +7,59 @@ class RestaurantPhotos extends React.Component {
     this.state = {
       showModal: false,
       currentModal: '',
+      amountToRender: this.getRandomNumber(),
     }
-    this.toggleModal = this.toggleModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
-  toggleModal(event) {
+  // toggleModal should bring up a modal window with the Modal that was clicked on being the 
+  // centerpiece
+
+  openModal(event) {
     const modalImage = event.target.src;
 
     this.setState(prevState => ({
-      showModal: !prevState.showModal,
-      currentModal: modalImage,
-    }));
+      showModal: true,
+      currentModal: modalImage
+    }))
+  }
 
-    console.log(this.state)
+  closeModal() {
+    this.setState(prevState => ({
+      showModal: false
+    }))
+  }
+
+  getRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
   }
 
   render() {
-    console.log(this.state)
-    if (this.props.isLoading) {
+    // OpenTable restaurants seem to be making a randomly generated photo mosaic per restaurant entry
+    // Therefore, restaurant carousel photo entries must be randomly generated per restaurant
+    // Everytime we render, we slice off a portion of the userPhotos to render, and randomly send to 
+    // display in the header
+    // const amountToRender = this.getRandomNumber();
+    const { photos, isLoading } = this.props
+    const { showModal, currentModal, amountToRender } = this.state
+
+    if (isLoading) {
       return (
         <div>
-          {this.props.photos.map((restaurant, index) => {
-            return restaurant.userPhotos.map((photo, index) => {
+          {photos.map((restaurant, index) => {
+            const photosToBeDisplayed = restaurant.userPhotos.slice(0, amountToRender);
+            return photosToBeDisplayed.map((photo, index) => {
               return (
-                <li><img src={photo.photoURL} key={index} onClick={this.toggleModal}></img></li>
+                <li><img src={photo.photoURL} key={index} onClick={this.openModal}></img></li>
               )
             })
           })}
+          {this.state.showModal ? <Modal onClose={this.closeModal} modalImage={this.state.currentModal} /> : null}
         </div>
       )
     } else {
-      return (
-        <h1>Loading...</h1>
-      )
+      return <h1>Loading...</h1>
     }
   }
 }
