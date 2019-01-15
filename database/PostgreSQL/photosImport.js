6e850d98c84ef13413198f557aa65033f5f3553f
cherrih <cherri.hartigan@gmail.com>
2019-01-14 18:51:15 -0800

    Successfully setup postgres

---
+++ database/PostgreSQL/photosImport.js
@@ -0,0 +1,38 @@
+const { Client } = require('pg');
+const path = require('path');
+
+const client = new Client({
+  host: 'localhost',
+  database: 'sdc',
+});
+
+const createPhotosTable = `
+  CREATE TABLE photos (
+    id INT NOT NULL PRIMARY KEY,
+    restaurant_id INT NOT NULL,
+    restaurant_name VARCHAR NOT NULL,
+    photo_description VARCHAR NOT NULL,
+    date VARCHAR NOT NULL,
+    username VARCHAR NOT NULL,
+    photoURL VARCHAR NOT NULL
+  );
+`;
+
+const importPhotosData = () => {
+  const fileName = `${path.join(__dirname, '../dataSmall.csv')}`;
+  const copyString = `COPY photos(id,restaurant_id,restaurant_name,photo_description,date,username,photoURL) FROM '${fileName}' DELIMITER ',' CSV HEADER;`;
+  client.query(copyString, (err) => {
+    if (err) console.log(err);
+    console.log('-------- succesfully imported data');
+    client.end();
+  });
+};
+
+client.connect()
+  .then(() => console.log('-- connected to database sdc'))
+  .then(() => client.query('DROP TABLE IF EXISTS photos'))
+  .then(() => client.query(createPhotosTable))
+  .then(() => console.log('---- created photos table'))
+  .then(() => console.log('------ beginning to import photos data'))
+  .then(() => importPhotosData())
+  .catch(err => console.log(err));
